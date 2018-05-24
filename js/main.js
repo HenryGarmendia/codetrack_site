f75f188a07e7197cd997339e4b2879a410e80ff1
HenryGarmendia <me@henrygarmendia.com>
2018-05-24 18:12:11 -0500

    added some hero-front image and planing to create make it more parallax

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   "use strict";
 
+  // STICKY HEADER
   let $header = $('header'),
-      $headerHeight = Math.round($header.height() + 286),
+      $headerHeight = Math.round($header.height() + 86),
       $sticky = $header.before($header.clone().addClass('header-main__sticky')),
       $stickyClass = document.querySelector('.header-main__sticky');
 
@@ -13,7 +14,24 @@ $(document).ready(function() {
       $($stickyClass).css({"transform": "translateY(-200%)"});
     }
   }; // stickyNav
-
   window.addEventListener('scroll', stickyNav);
+
+  // PARALLAX BACKGROUND PICTURES
+  let $headerMain = Math.round($(".header-main").height());
+
+  $(window).scroll(function(){
+    let winScroll = $(this).scrollTop();
+
+    if(winScroll <= $headerMain){
+      $(".header-main__hero-wrapper--hero-bkg").css({
+        // "transform": "translateY(" + winScroll / 2 + "px)"
+      });
+
+      $(".header-main__hero-wrapper--hero-front").css({
+        "transform": "translateY(" + winScroll / 2 + "px)"
+      });
+    }
+
+  }); // $(window).scroll
 
 }); // document.ready
